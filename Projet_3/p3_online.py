a79dc6a3b0bd637b5b1dcc7dad8f68222eaf93e3
pfroide <p.froide@gmail.com>
2018-02-17 10:46:14 +0100

    Version online du fichier avec bug des id numériques

--- Projet_3/p3_online.py
+++ Projet_3/p3_online.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Created on Wed Feb 14 20:22:34 2018
 
@@ -9,103 +8,42 @@ import numpy as np
 import pandas as pd
 from sklearn.neighbors import NearestNeighbors
 from sklearn import preprocessing
-from flask import Flask  # pip install flask
+from flask import Flask
+import json
 
 # Lieu où se trouve le fichier
-_FICHIER = 'C:\\Users\\Toni\\Desktop\\pas_synchro\\p3_bdd_clean_v2.csv'
-_DOSSIERTRAVAIL = 'C:\\Users\\Toni\\python\\python\\Projet_3\\images'
+_DOSSIER = 'C:\\Users\\Toni\\Desktop\\pas_synchro\\'
+_FICHIERDATA = _DOSSIER + 'p3_bdd_clean_v2.csv'
+_FICHIERDATANUM = _DOSSIER + 'p3_datanum.csv' 
 
 app = Flask(__name__)
 
-@app.route("/")
+#@app.route('/recommand/<id_film>')
+@app.route('/<id_film>')
 
-def hello():
-    return "  World!"
+def recommand(id_film): 
 
-if __name__ == "__main__":
-    app.run()
+    data = pd.read_csv(_FICHIERDATA, encoding="ISO-8859-1")
+    datanum = pd.read_csv(_FICHIERDATANUM, encoding="ISO-8859-1")
+    data = data.replace({'\xa0': ''}, regex=True)
+    del data['Unnamed: 0']
+    del datanum['Unnamed: 0']
 
-def count_word(data, ref_col, liste):
-    """
-    TBD
-    """
-    keyword_count = dict()
+    #print(recommandation(datanum, data, id_film))
 
-    for word in liste:
-        keyword_count[word] = 0
-
-    for liste_keywords in data[ref_col].str.split('|'):
-        if isinstance(liste_keywords, float) and pd.isnull(liste_keywords):
-            continue
-        for word in [word for word in liste_keywords if word in liste]:
-            if pd.notnull(word):
-                keyword_count[word] = keyword_count[word] + 1
-
-    # convert the dictionary in a list to sort the keywords by frequency
-    keyword_occurences = []
-
-    for k, v in keyword_count.items():
-        keyword_occurences.append([k, v])
-
-    keyword_occurences.sort(key=lambda x: x[1], reverse=True)
-
-    return keyword_occurences, keyword_count
-
-def comptabiliser(data, valeur_cherchee):
-    """
-    TBD
-    """
-    # compter tous les genres différents
-    listing = set()
-
-    for word in data[valeur_cherchee].str.split('|').values:
-        if isinstance(word, float):
-            continue
-        listing = listing.union(word)
-
-    # compter le nombre d'occurence de ces genres
-    listing_compte, dum = count_word(data, valeur_cherchee, listing)
-
-    return listing_compte
-
-def transpose_bool(data, colon, limite):
-    """
-    TBD
-    """
-
-    # On supprime les #NA
-    data[colon].fillna('vide', inplace=True)
-
-    # énumaration des genres
-    listing = comptabiliser(data, colon)
-
-    p = 0
-
-    for mot, compte in listing:
-        if p < limite:
-            if mot not in data:
-                data[mot] = pd.Series(((1 if mot in data[colon][i] else 0) for i in range(len(data[colon]))), index=data.index)
-            else:
-                data[mot] = pd.Series(((1 if (np.logical_or(data[mot].item == 1, mot in data[colon][i])) else 0) for i in range(len(data[colon]))), index=data.index)
-        else:
-            return p
-        p = p+1
-
-    return p
+    return recommandation(datanum, data, id_film)
 
 def recommandation(datanum, data, id_film):
     """
     TBD
     """
+
     #
-    if isinstance(id_film, int):
-        data_film = datanum.loc[id_film].values.reshape(1, -1)
-        # Quel est le titre recherché
-        titre = data['movie_title'].loc[id_film]
-        titre_fin = titre
-        print('Titre retenu : ', titre)
-    else:
-        
+    texte_final = ''
+
+    #
+    if isinstance(id_film, str):
+
         # Rajout des noms
         datanum['movie_title'] = data['movie_title']
 
@@ -115,16 +53,26 @@ def recommandation(datanum, data, id_film):
         # Quel est le titre recherché
         mask = datanum['movie_title'].str.contains(id_film)
         titre = data['movie_title'][mask]
-        titre_fin = str(titre.values[-1:])
-        print('Titre retenu : ', titre.values[-1:])
+
+        # Astuce pour avoir un string au bon format
+        for i in titre[-1:]:
+            titre_fin = i
 
         # Suppression de la colonne non-chifrée
         del data_film['movie_title']
         del datanum['movie_title']
 
+    else:
+        data_film = datanum.loc[int(id_film)].values.reshape(1, -1)
+        # Quel est le titre recherché
+        titre_fin = data['movie_title'].loc[int(id_film)]
+
     data_film = np.array(data_film)
 
     if data_film.size > 0:
+
+        # Nom du film retenu
+        texte_final = 'Titre retenu : ' + titre_fin + '<br>'
 
         # configuration du knn
         neigh = NearestNeighbors(n_neighbors=20,
@@ -139,23 +87,24 @@ def recommandation(datanum, data, id_film):
         indice_supp = []
 
         for i in indices[1][-1]:
-            #print(data.loc[i]['movie_title'])
-            if ((str(data.loc[i]['movie_title']) in titre_fin)):
+            if str(data.loc[i]['movie_title']) in titre_fin:
                 indice_supp.append(i)
             elif (titre_fin in (str(data.loc[i]['movie_title']))):
                 indice_supp.append(i)
 
         #
         for i in indices[1]:
-            second_df = data.loc[i] 
-        
+            second_df = data.loc[i]
+
         for i in indice_supp:
             second_df = second_df.drop([i])
-        
+
         # Création du second dataset pour tester la popularité
         liste_criteres = ['movie_title',
                           'cast_total_facebook_likes',
                           'imdb_score',
+                          'num_user_for_reviews',
+                          'num_voted_users',
                           'movie_facebook_likes']
 
         for colon in second_df:
@@ -166,10 +115,13 @@ def recommandation(datanum, data, id_film):
         second_df.fillna(0, inplace=True)
 
         # Tester la popularité
-        pop(second_df, data, datanum, id_film)
+        reponse = pop(second_df, data, datanum, id_film)
+        texte_final = texte_final + reponse
 
     else:
-        print("Le film recherché n'existe pas.")
+        texte_final = "Le film recherché n'existe pas."
+
+    return texte_final
 
 def pop(second_df, data, datanum, id_film):
     """
@@ -180,9 +132,11 @@ def pop(second_df, data, datanum, id_film):
     min_max_scaler = preprocessing.MinMaxScaler()
     second_df[['cast_total_facebook_likes',
                'imdb_score',
-               'movie_facebook_likes']] = min_max_scaler.fit_transform(second_df[['cast_total_facebook_likes', 'imdb_score', 'movie_facebook_likes']])
-    
-    second_df['score'] = second_df['cast_total_facebook_likes'] + second_df['imdb_score'] + second_df['movie_facebook_likes']
+               'num_user_for_reviews',
+               'num_voted_users',
+               'movie_facebook_likes']] = min_max_scaler.fit_transform(second_df[['cast_total_facebook_likes', 'imdb_score', 'num_user_for_reviews', 'num_voted_users', 'movie_facebook_likes']])
+
+    second_df['score'] = second_df['cast_total_facebook_likes'] + second_df['imdb_score'] + second_df['movie_facebook_likes'] + second_df['num_user_for_reviews'] + second_df['num_voted_users']
 
     # Score du film
     score = sum(second_df.score[second_df.movie_title == id_film])
@@ -191,78 +145,20 @@ def pop(second_df, data, datanum, id_film):
     second_df['score'] = abs(second_df['score']-score)
     second_df = second_df.sort_values(by='score', ascending=True)
     
-    # 5 résultats les plus proches sans prendre le film lui-même qui est en case 0
-    res = second_df['movie_title'][1:6]
-
-    # Résultats
-    print(res)
-
-    # Petite vérification des mot-clefs associés
-    for i in range(len(res)):
-        print(data.loc[res.index[i]]['movie_title'])
-        for j in datanum:
-            if datanum.loc[res.index[i]][j] == 1:
-                print(j)
-        print()
-        # datanum.loc[n][datanum.loc[n] == 1]
-
-def second(second_df):
-    """
-    TBD
-    """
-
-    liste_criteres = ['cast_total_facebook_likes',
-                      'num_user_for_reviews',
-                      'imdb_score',
-                      'movie_facebook_likes']
-
-    for colon in second_df:
-        if colon not in liste_criteres:
-            del second_df[colon]
-
-def premain():
-    """
-    TBD
-    """
+    # 5 résultats
+    second_df = second_df[0:5]
     
-    #data_test.set_index("Robert de Niro", inplace=True)
-    # On charge le dataset
-    data = pd.read_csv(_FICHIER, encoding="ISO-8859-1")
+    # Résultats en dictionnaire
+    #dico_resultats = {}
 
-    # Suppression de la colonne inutile
-    del data['Unnamed: 0']
+    # 5 résultats les plus proches sans prendre le film lui-même qui est en case 0
+    #for i in range(0,5):
+    #    dico_resultats[str(second_df.index[i])] = second_df['movie_title'][second_df.index[i]]
 
-    # Suppression des espaces en fin de chaine de caractères
-    data['movie_title'] = data['movie_title'].replace({'\xa0': ''}, regex=True)
-    data['movie_title'] = data['movie_title'].str.strip()
+    # 5 résultats les plus proches sans prendre le film lui-même
+    dico = {"_results" :[{'id':int(key),"name":value} for key, value in second_df['movie_title'].items()]}
+    
+    json_dico = json.dumps(dico, indent=4, separators=(',', ': '))
 
-    datanum = data.copy()
-    datanum.describe()
-
-    # Données manquantes
-    missing_data = datanum.isnull().sum(axis=0).reset_index()
-    missing_data.columns = ['column_name', 'missing_count']
-    missing_data['filling_factor'] = (datanum.shape[0]-missing_data['missing_count'])/datanum.shape[0]*100
-    missing_data.sort_values('filling_factor').reset_index(drop=True)
-
-    # Transposition en 0 et 1 des valeurs non-numériques
-    liste_criteres = ['actor_1_name',
-                      'actor_2_name',
-                      'actor_3_name',
-                      'genres',
-                      'content_rating',
-                      'director_name']
-
-    for critere in liste_criteres:
-        num = transpose_bool(datanum, critere, 100)
-        print("Nombre : ", num, "\t", critere)
-
-    # Suprresion de ce qui n'est pas chiffré
-    datanum = datanum.drop(['color', 'director_name', 'actor_1_name', 'genres', 'movie_title', 'actor_2_name', 'actor_3_name'], axis=1)
-    datanum = datanum.drop(['plot_keywords', 'movie_imdb_link', 'language', 'country', 'content_rating'], axis=1)
-
-    # Suprresion de ce qui n'est pas chiffré #2
-    datanum = datanum.drop(['num_critic_for_reviews', 'director_facebook_likes', 'actor_3_facebook_likes'], axis=1)
-    datanum = datanum.drop(['num_user_for_reviews', 'actor_1_facebook_likes', 'actor_2_facebook_likes'], axis=1)
-    datanum = datanum.drop(['aspect_ratio', 'num_voted_users', 'cast_total_facebook_likes'], axis=1)
-    datanum = datanum.drop(['title_year', 'gross', 'duration', 'budget', 'imdb_score', 'movie_facebook_likes', 'facenumber_in_poster'], axis=1)
+    # print(dico_resultats)
+    return(json_dico)
