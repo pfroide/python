9e775b4d6a5d917a52a24e06a7554c0a93436f5c
pfroide <p.froide@gmail.com>
2018-05-08 19:25:34 +0200

    Mise à jour mineure

--- Projet_5/p5_testclient.py
+++ Projet_5/p5_testclient.py
@@ -6,95 +6,98 @@ Created on Thu Apr 26 20:16:04 2018
 """
 
 import warnings
-import datetime
 import random as rd
 import pandas as pd
 
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.externals import joblib
 
 # Pour ne pas avoir les warnings lors de la compilation
 warnings.filterwarnings("ignore")
 
 # Lieu où se trouve le fichier
 _DOSSIER = 'C:\\Users\\Toni\\Desktop\\pas_synchro\\p5\\'
+_DOSSIERPKL = 'C:\\Users\\Toni\\python\\python\\Projet_5\\pkl'
 _FICHIERDATA = _DOSSIER + 'dataset_p5.csv'
 
-def creation_client(data):
+def creation_factures(data, nb_factures):
     """
-    Fonction pour créer le client mystère
+    Fonction pour créer les factures mystères
     """
-
-    # 1 ligne = 1 client
-    row_c = 0
 
     # Création d'un dataframe vide avec les bonnes colonnes
     df_cli = pd.DataFrame(columns=data.columns)
     del df_cli['labels']
 
-    # Tirage au sort des valeurs de la facture
-    df_cli.loc[row_c, 'nb_factures'] = 1
-    df_cli.loc[row_c, 'somme_total'] = rd.randint(2, 500)
-    df_cli.loc[row_c, 'nb_categorie_total'] = rd.randint(1, 500)
-    df_cli.loc[row_c, 'nb_article_total'] = rd.randint(1, 100)
-    df_cli.loc[row_c, 'day_of_week'] = rd.randint(0, 6)
-    df_cli.loc[row_c, 'interval_jour_achat_n1'] = rd.randint(0, 12)
-    df_cli.loc[row_c, 'interval_heure_achat_n1'] = rd.randint(0, 4)
-    df_cli.loc[row_c, 'interval_moyenne_horaire'] = rd.randint(0, 4)
-    df_cli.loc[row_c, 'valeur_facture_1'] = df_cli.loc[row_c, 'somme_total']
+    for i in range(0, nb_factures):
 
-    # Déduction des autres
-    df_cli.loc[row_c, 'mean_nb_article_facture'] = (
-        df_cli.loc[row_c, 'nb_article_total']/df_cli.loc[row_c, 'nb_factures'])
+        print('\nFacture n°', i)
 
-    df_cli.loc[row_c, 'mean_somme_par_facture'] = (
-        df_cli.loc[row_c, 'somme_total']/df_cli.loc[row_c, 'nb_factures'])
+        # 1 ligne = 1 facture
+        row_c = i
 
-    df_cli.loc[row_c, 'mean_nb_categorie_facture'] = (
-        df_cli.loc[row_c, 'nb_categorie_total']/df_cli.loc[row_c, 'nb_factures'])
+        # Tirage au sort des valeurs de la facture
+        df_cli.loc[row_c, 'nb_factures'] = 1
+        df_cli.loc[row_c, 'somme_total'] = rd.randint(2, 500)
+        df_cli.loc[row_c, 'nb_categorie_total'] = rd.randint(1, 500)
+        df_cli.loc[row_c, 'nb_article_total'] = rd.randint(1, 100)
+        df_cli.loc[row_c, 'day_of_week'] = rd.randint(0, 6)
+        df_cli.loc[row_c, 'interval_jour_achat_n1'] = rd.randint(0, 12)
+        df_cli.loc[row_c, 'interval_heure_achat_n1'] = rd.randint(0, 4)
+        df_cli.loc[row_c, 'interval_moyenne_horaire'] = rd.randint(0, 4)
+        df_cli.loc[row_c, 'valeur_facture_1'] = df_cli.loc[row_c, 'somme_total']
 
-    df_cli.loc[row_c, 'mean_somme_par_article'] = (
-        df_cli.loc[row_c, 'somme_total']/df_cli.loc[row_c, 'nb_article_total'])
+        # Déduction des autres
+        df_cli.loc[row_c, 'mean_nb_article_facture'] = (
+            df_cli.loc[row_c, 'nb_article_total']/df_cli.loc[row_c, 'nb_factures'])
 
-    df_cli.loc[row_c, 'ecart_moy_2_achats'] = 365
-    df_cli.loc[row_c, 'ecart_min_2_achats'] = 365
-    df_cli.loc[row_c, 'ecart_max_2_achats'] = 365
+        df_cli.loc[row_c, 'mean_somme_par_facture'] = (
+            df_cli.loc[row_c, 'somme_total']/df_cli.loc[row_c, 'nb_factures'])
 
-#    # Tirage au sort de la date.
-#    startdate = datetime.date(2010, 12, 1)
-#    date = startdate + datetime.timedelta(rd.randint(1, 365))
+        df_cli.loc[row_c, 'mean_nb_categorie_facture'] = (
+            df_cli.loc[row_c, 'nb_categorie_total']/df_cli.loc[row_c, 'nb_factures'])
 
-    # Affichage de confirmation
-    for i in df_cli:
-        print(i, "\t", df_cli.loc[row_c, i])
+        df_cli.loc[row_c, 'mean_somme_par_article'] = (
+            df_cli.loc[row_c, 'somme_total']/df_cli.loc[row_c, 'nb_article_total'])
+
+        df_cli.loc[row_c, 'ecart_moy_2_achats'] = 365
+        df_cli.loc[row_c, 'ecart_min_2_achats'] = 365
+        df_cli.loc[row_c, 'ecart_max_2_achats'] = 365
+
+        # Affichage de confirmation
+        for j in df_cli:
+            print(j, "\t", df_cli.loc[row_c, j])
 
     return df_cli
 
-def main():
+def prediction_classe(df_cli, rfc):
+    """
+    Fonction pour prédire la classe du client pour chaque facture
+    """
+
+    for i in range(0, len(df_cli)):
+        resultat = rfc.predict(df_cli.loc[i].reshape(1, -1))
+        print('\nFacture n°', i, '\nLe client appartient à la classe n°', resultat)
+
+def main(nb_factures):
     """
     Fonction principale
     """
 
     # Lecture du dataset
     data = pd.read_csv(_FICHIERDATA, error_bad_lines=False)
+    del data['Unnamed: 0']
 
-    # Récupération de l'index
-    data = data.set_index('Unnamed: 0')
+    # Nom du classifieur
+    name = 'RandomForestClassifier'
 
-    # Axe X
-    data_x = data.copy()
+    # Localisation de du fichier du fit sauvegardé
+    fichier = _DOSSIERPKL + "\\" + name + ".pkl"
 
-    # On supprime les étiquettes de l'axe X
-    del data_x['labels']
+    # On va chercher le dump
+    rfc = joblib.load(fichier)
 
-    # Axe Y = étiquettes
-    data_y = data['labels']
-
-    # Répartition Train/Test
-    xtrain, xtest, ytrain, ytest = train_test_split(data_x, data_y, train_size=0.75)
-
-    # Fit avec le classifieur choisi
-    rfc = RandomForestClassifier().fit(xtrain, ytrain)
+    # Création aléatoire de factures
+    df_cli = creation_factures(data, nb_factures)
 
     # Déduction du label et affichage
-    print("\nClasse du client :", rfc.predict(creation_client(data))[0])
+    prediction_classe(df_cli, rfc)
