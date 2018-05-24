9b0a708115f218cdd0a8132cbc032f6f499c1880
pfroide <p.froide@gmail.com>
2018-05-25 00:14:53 +0200

    Rajout de la fonction de comptage de l'occurence des mots

--- Projet_6/p6_nettoyage.py
+++ Projet_6/p6_nettoyage.py
@@ -11,28 +11,51 @@ import numpy as np
 import pandas as pd
 #nltk.download()
 
-from nltk.stem.porter import *
 from bs4 import BeautifulSoup
-from matplotlib import pyplot as plt, cm as cm
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from nltk.tokenize.moses import MosesDetokenizer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
+from sklearn.decomposition import NMF
+from matplotlib import pyplot as plt, cm as cm
+from yellowbrick.text import FreqDistVisualizer
+#from wordcloud import WordCloud
 
 # Lieu où se trouve le fichier
 _DOSSIER = 'C:\\Users\\Toni\\Desktop\\pas_synchro\\p6\\'
 _DOSSIERTRAVAIL = 'C:\\Users\\Toni\\python\\python\\Projet_6\\images'
 
-def creer_matrix(text):
+def creer_tfidfvectorizer(text):
     """
-    TBD
+    Fonction de création de la matrice numérique
+    Comptage de fréquence
     """
 
+    # Création de l'objet
     vectorizer = CountVectorizer()
-    t_vec = TfidfVectorizer()
 
-    X = vectorizer.fit_transform(text)
-    Y = t_vec.fit_transform(text)
+    # Fit du texte d'entrée, et mis au format tableau
+    liste_mots = vectorizer.fit_transform(text).toarray()
+
+    # On ressort le tableau, et la liste des mots
+    return liste_mots, vectorizer.get_feature_names()
+
+def creer_countvectorizer(text):
+    """
+    Fonction de création de la matrice
+    Comptage global d'occurence
+    """
+
+    # Création de l'objet
+    t_vectorizer = TfidfVectorizer()
+
+    # Fit du texte d'entrée, et mis au format tableau
+    liste_mots = t_vectorizer.fit_transform(text).toarray()
+
+    # On ressort le tableau, et la liste des mots
+    return liste_mots, t_vectorizer.get_feature_names()
 
     #print(X.toarray())
     #print(Y.toarray())
@@ -40,40 +63,49 @@ def creer_matrix(text):
     #print(vectorizer.get_feature_names())
     #print(t_vec.get_feature_names())
 
-    return X.toarray()
-
     #dense = Y.todense()
     #denselist = dense.tolist()
     #df = pd.DataFrame(denselist)
     #s = pd.Series(df.loc[0])
     #s[s > 0].sort_values(ascending=False)
 
-def fct_nltk(text):
+def fct_nltk(text, stop_words):
     """
-    TBD
+    Fonction pour supprimer :
+        les step words
+        la ponctuation
+        les majuscules
+        les pluriels
     """
 
+    # Création de l'objet
     lemma = nltk.wordnet.WordNetLemmatizer()
 
-    stop_words = set(stopwords.words('english'))
+    # Tokenization et mise en minuscule
     words = word_tokenize(text.lower())
 
-    new_sentence = [lemma.lemmatize(x) for x in words if ((not x in stop_words) and x.isalpha())]
+    # Suppression des pluriels et de la ponctuation. Boule pour toutes les lignes
+    new_sentence = [lemma.lemmatize(x) for x in words if (not x in stop_words) and x.isalpha()]
 
+    # Sortie
     return new_sentence
 
 def suppr_html_code(fichier):
     """
-    TBD
+    Fonction de suppression des balises HTML et des parties de code
     """
 
+    # Suppression des balises HTML
     soupe = BeautifulSoup(fichier, "lxml")
 
+    # Recherche des balises de 'code'
     liste = soupe.findAll('code')
 
+    # Suppression des données qui sont entre les balises de code
     for balise in liste:
         balise.decompose()
 
+    # Sortie formatée en texte
     return soupe.text
 
 def donnees_manquantes(data, nom):
@@ -95,6 +127,77 @@ def donnees_manquantes(data, nom):
     print(data_transpose)
     data_transpose.to_csv(fichier_save)
 
+def display_topics(model, feature_names, no_top_words):
+    """
+    TBD
+    """
+
+    for topic_idx, topic in enumerate(model.components_):
+        print("Topic", topic_idx)
+        print(" ".join([feature_names[i] for i in topic.argsort()[:-no_top_words - 1:-1]]))
+
+def reduction_dimension(data):
+    """
+    TBD
+    """
+
+    from sklearn.decomposition import TruncatedSVD
+
+    for i in [500, 1500, 2500, 3000]:
+        svd = TruncatedSVD(n_components=i,
+                           n_iter=7,
+                           random_state=42)
+
+        svd.fit(data)
+
+        #print(svd.explained_variance_ratio_)
+        print(i)
+        print(round(100*svd.explained_variance_ratio_.sum(), 2))
+        #print(svd.singular_values_)
+
+#def generate_wordcloud(data):
+#    """
+#    Simple WordCloud
+#    """
+#
+#    wordcloud = WordCloud(background_color='black',
+#                          #stopwords=stopwords,
+#                          max_words=20,
+#                          max_font_size=20,
+#                          relative_scaling=1,
+#                          min_font_size=1,
+#                          scale=3,
+#                          random_state=1 # chosen at random by flipping a coin; it was heads
+#                         ).generate(str(data))
+#
+#    fig = plt.figure(1, figsize=(12, 12))
+#    plt.axis('off')
+#
+#    #if title:
+#    #    fig.suptitle(title, fontsize=20)
+#    #    fig.subplots_adjust(top=2.3)
+#
+#    plt.imshow(wordcloud)
+#    plt.show()
+
+def comptage(data):
+    """
+    TBD
+    """
+    import collections
+
+    count = collections.Counter()
+
+    stop_words = set(stopwords.words('english'))
+
+    for sentence in data:
+        sentence = word_tokenize(sentence.lower())
+        for word in sentence:
+            if (not word in stop_words) and word.isalpha():
+                count[word] += 1
+
+    return count
+
 def main():
     """
     Fonction principale
@@ -105,16 +208,87 @@ def main():
     data = pd.read_csv(_DOSSIER + fichier, error_bad_lines=False)
 
     #
-    data = data[0:500]
+    data = data[0:10000]
 
-    #
+    # Nouveau dataframe qui prendra les résultats en entrée
     new_df = pd.DataFrame()
-    matrix_num = pd.DataFrame()
 
     # Données manquantes
     donnees_manquantes(data, "missing_data_1")
 
-    #
+    # Suppression des balises html et des parties de code
     data['Body'] = [suppr_html_code(x) for x in data['Body']]
-    new_df['Sentences'] = [fct_nltk(x) for x in data['Body']]
-    matrix_num['numbers'] = [creer_matrix(x) for x in new_df['Sentences']]
+
+    # Comptage du nombre d'occurence
+    cpt = comptage(data['Body'])
+
+    # Liste des stop words anglais
+    least_used = set([word for word in cpt if cpt[word] < 5])
+
+    stop_words = set(stopwords.words('english')) \
+                | set([word for word, freq, in cpt.most_common(100)]) \
+                | least_used
+
+    # Suppression des pluriels et des stop words
+    # Rétrecissment du dataset
+    new_df['Sentences'] = [fct_nltk(x, stop_words) for x in data['Body']]
+
+    # On est obligé de detokenizer pour créer la matrice finale
+    detokenizer = MosesDetokenizer()
+    new_df['Sentences'] = [detokenizer.detokenize(x, return_str=True) for x in new_df['Sentences']]
+
+    # Création de la matrice finale
+    matrixnum_count, names_count = creer_countvectorizer(new_df['Sentences'])
+    # Conversion de la matrice finale en dataframe pour facilité d'usage
+    matrixnum_count = pd.DataFrame(matrixnum_count, columns=names_count)
+
+    # Run LDA
+    lda = LatentDirichletAllocation(n_components=20,
+                                    max_iter=5,
+                                    learning_method='online',
+                                    learning_offset=50,
+                                    random_state=0)
+
+    #
+    lda.fit(matrixnum_count)
+
+    #
+    display_topics(lda, names_count, 10)
+
+    #
+    #generate_wordcloud(matrixnum_count)
+    #| set([word for word, freq, in count.most_common(100)])
+
+    # Test de visualisation
+    visualizer = FreqDistVisualizer(features=names_count,
+                                    n=25,
+                                    orient='v',
+                                    color='g')
+    visualizer.fit(matrixnum_count)
+    visualizer.poof()
+
+    # Création de la matrice finale
+    matrixnum_tfidf, names_tfidf = creer_tfidfvectorizer(new_df['Sentences'])
+    # Conversion de la matrice finale en dataframe pour facilité d'usage
+    matrixnum_tfidf = pd.DataFrame(matrixnum_tfidf, columns=names_tfidf)
+
+    # Run NMF
+    nmf = NMF(n_components=20,
+              random_state=1,
+              alpha=.1,
+              l1_ratio=.5,
+              init='nndsvd')
+
+    #
+    nmf.fit(matrixnum_tfidf)
+
+    #
+    display_topics(nmf, names_tfidf, 10)
+
+    # Test de visualisation
+    visualizer = FreqDistVisualizer(features=names_tfidf,
+                                    n=25,
+                                    orient='v',
+                                    color='g')
+    visualizer.fit(matrixnum_tfidf)
+    visualizer.poof()
