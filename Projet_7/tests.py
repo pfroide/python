df25e81b2d37f6a0a9e56a561da39cd107c2e0fb
toni <p.froide@gmail.com>
2018-10-20 12:57:47 +0200

    Quelques tests supplémentaires

--- Projet_7/tests.py
+++ Projet_7/tests.py
@@ -9,53 +9,76 @@ import keras
 import os
 import tensorflow as tf
 
-os.environ["CUDA_VISIBLE_DEVICES"]=""
-
-config = tf.ConfigProto(device_count = {'GPU': 0})
-sess = tf.Session(config=config)
-
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D
-
-my_VGG16 = Sequential()  # Création d'un réseau de neurones vide
-
-# Ajout de la première couche de convolution, suivie d'une couche ReLU
-my_VGG16.add(Conv2D(64, (3, 3), input_shape=(224, 224, 3), padding='same', activation='relu'))
-
-# Ajout de la deuxième couche de convolution, suivie  d'une couche ReLU
-my_VGG16.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
-
-# Ajout de la première couche de pooling
-my_VGG16.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
-
-from keras.layers import Flatten, Dense
-
-my_VGG16.add(Flatten())  # Conversion des matrices 3D en vecteur 1D
-
-# Ajout de la première couche fully-connected, suivie d'une couche ReLU
-my_VGG16.add(Dense(4096, activation='relu'))
-
-# Ajout de la deuxième couche fully-connected, suivie d'une couche ReLU
-my_VGG16.add(Dense(4096, activation='relu'))
-
-# Ajout de la dernière couche fully-connected qui permet de classifier
-my_VGG16.add(Dense(1000, activation='softmax'))
-
 from keras.applications.vgg16 import VGG16
-
-model = VGG16() # Création du modèle VGG-16 implementé par Keras
-
+from keras.applications.vgg16 import decode_predictions
 from keras.preprocessing.image import load_img, img_to_array
 from keras.applications.vgg16 import preprocess_input
 
-img = load_img('cat.jpg', target_size=(224, 224))  # Charger l'image
-img = img_to_array(img)  # Convertir en tableau numpy
-img = img.reshape((1, img.shape[0], img.shape[1], img.shape[2]))  # Créer la collection d'images (un seul échantillon)
-img = preprocess_input(img)  # Prétraiter l'image comme le veut VGG-16
+model = VGG16() # Création du modèle VGG-16 implementé par Keras
 
-y = model.predict(img)  # Prédir la classe de l'image (parmi les 1000 classes d'ImageNet)
+def training():
+    """
+    TBD
+    """
 
-from keras.applications.vgg16 import decode_predictions
+    # VGG-16 reçoit des images de taille (224, 224, 3)
+    # la fonction  load_img  permet de charger l'image et de la redimensionner
+    # correctement
+    img = load_img('/home/toni/Bureau/saul-5.jpg', target_size=(224, 224))  # Charger l'image
 
-# Afficher les 3 classes les plus probables
-print('Top 3 :', decode_predictions(y, top=3)[0])
+    # Keras traite les images comme des tableaux numpy
+    # img_to_array  permet de convertir l'image chargée en tableau numpy
+    img = img_to_array(img)  # Convertir en tableau numpy
+
+    # Le réseau doit recevoir en entrée une collection d'images,
+    # stockée dans un tableau de 4 dimensions, où les dimensions correspondent
+    # (dans l'ordre) à (nombre d'images, largeur, hauteur, profondeur).
+    # Pour l'instant, nous donnons qu'une image en entrée : numpy.reshape
+    # permet d'ajouter la première dimension (nombre d'images = 1) à notre image.
+    img = img.reshape((1, img.shape[0], img.shape[1], img.shape[2]))
+
+    # Enfin,  preprocess_input  permet d'appliquer les mêmes pré-traitements
+    # que ceux utilisés sur l'ensemble d'apprentissage lors du pré-entraînement.
+    img = preprocess_input(img)  # Prétraiter l'image comme le veut VGG-16
+
+    # Prédire la classe de l'image (parmi les 1000 classes d'ImageNet)
+    y = model.predict(img)
+
+    # Afficher les 3 classes les plus probables
+    print('Top 3 :', decode_predictions(y, top=3)[0])
+
+
+def transfert_learning():
+    """
+    TBD
+    """
+
+    # Stratégie #1 : fine-tuning total
+    # Ici, on entraîne tout le réseau, donc il faut rendre toutes les
+    # couches "entraînables" :
+
+    for layer in model.layers:
+       layer.trainable = True
+
+    # Stratégie #2 : extraction de features
+    # On entraîne seulement le nouveau classifieur et on ne ré-entraîne pas
+    # les autres couches :
+
+    for layer in model.layers:
+       layer.trainable = False
+
+    # Stratégie #3 : fine-tuning partiel
+    # On entraîne le nouveau classifieur et les couches hautes :
+
+    # Ne pas entraîner les 5 premières couches (les plus basses)
+    for layer in model.layers[:5]:
+       layer.trainable = False
+
+
+    #  Entraînement du réseau
+    # Il ne reste plus qu'à compiler le nouveau modèle, puis à l'entraîner  :
+    # Compiler le modèle
+    new_model.compile(loss="categorical_crossentropy", optimizer=optimizers.SGD(lr=0.0001, momentum=0.9), metrics=["accuracy"])
+
+    # Entraîner sur les données d'entraînement (X_train, y_train)
+    model_info = new_model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, verbose=2)
