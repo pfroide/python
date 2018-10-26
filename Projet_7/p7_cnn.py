dfe924f1a3ef9a9d6025723a927a3b2fd86a2de7
toni <p.froide@gmail.com>
2018-10-27 00:06:27 +0200

    Mise à jour de forme

--- Projet_7/p7_cnn.py
+++ Projet_7/p7_cnn.py
@@ -7,6 +7,7 @@ Created on Mon Oct 19 22:19:42 2018
 """
 import os
 import random
+import time
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -19,17 +20,17 @@ from tqdm import tqdm
 import cv2
 
 # Lieu où se trouvent des images
-IMG_DIR = '/home/toni/Bureau/p7/flow/'
-DOSSIER_SOURCE = '/home/toni/Bureau/p7/Images/'
+DOSSIER_SOURCE = '/home/toni/Bureau/p7/flow/'
+IMG_DIR = '/home/toni/Bureau/p7/Images/'
 
 # Définitions des limites d'execution
-NB_RACES = 3
-NB_EXEMPLES = 100
-T_IMG = 90
+NB_RACES = 5
+NB_EXEMPLES = 300
+T_IMG = 224
 BATCH_SIZE = 32
-DATA_AUGMENTATION = True
+DATA_AUGMENTATION = False
 
-def calcul_resultats(res, test_y, classifieur):
+def cnn_calcul_resultats(res, test_y, classifieur):
     """
     Fonction qui va calculer les pourcentages de bons pronostics
     """
@@ -60,19 +61,21 @@ def calcul_resultats(res, test_y, classifieur):
 
     print(data_resultats)
 
-def data_augmentation(model, liste_train, liste_test):
+def cnn_data_augmentation(model, liste_train, liste_test):
     """
     Fonction pour la data augmentation
     """
 
     # DA pour le training
-    train_datagen = keras.preprocessing.image.ImageDataGenerator(rescale=1./255,
+    train_datagen = keras.preprocessing.image.ImageDataGenerator(rotation_range=20,
+                                                                 width_shift_range=0.2,
+                                                                 height_shift_range=0.2,
                                                                  shear_range=0.2,
                                                                  zoom_range=0.2,
                                                                  horizontal_flip=True)
 
     train_generator = train_datagen.flow_from_dataframe(dataframe=liste_train,
-                                                        directory=IMG_DIR,
+                                                        directory=DOSSIER_SOURCE,
                                                         x_col='liste',
                                                         y_col='labels',
                                                         has_ext=True,
@@ -81,10 +84,10 @@ def data_augmentation(model, liste_train, liste_test):
                                                         class_mode='categorical')
 
     # DA pour la validation
-    valid_datagen = keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+    valid_datagen = keras.preprocessing.image.ImageDataGenerator()
 
     valid_generator = valid_datagen.flow_from_dataframe(dataframe=liste_test,
-                                                        directory=IMG_DIR,
+                                                        directory=DOSSIER_SOURCE,
                                                         x_col='liste',
                                                         y_col='labels',
                                                         has_ext=True,
@@ -94,13 +97,13 @@ def data_augmentation(model, liste_train, liste_test):
 
     hist = model.fit_generator(train_generator,
                                steps_per_epoch=2000 // BATCH_SIZE,
-                               epochs=100,
+                               epochs=25,
                                validation_data=valid_generator,
                                validation_steps=200 // BATCH_SIZE)
 
     return hist
 
-def appel_vgg(x_train, y_train, x_valid, y_valid,liste_train, liste_test):
+def cnn_appel_vgg(x_train, y_train, x_valid, y_valid,liste_train, liste_test):
     """
     Fonction de transfert learning
     """
@@ -134,7 +137,7 @@ def appel_vgg(x_train, y_train, x_valid, y_valid,liste_train, liste_test):
     model.summary()
 
     if DATA_AUGMENTATION is True:
-        res = data_augmentation(model, liste_train, liste_test)
+        res = cnn_data_augmentation(model, liste_train, liste_test)
     else:
         # Entrainement
         res = model.fit(x_train,
@@ -144,11 +147,11 @@ def appel_vgg(x_train, y_train, x_valid, y_valid,liste_train, liste_test):
                         verbose=1)
 
     # Tracé de courbes pour visualiser les résultats
-    courbes(res)
+    cnn_courbes(res)
 
     return model
 
-def courbes(resultat):
+def cnn_courbes(resultat):
     """
     Tracé des courbes d'accuracy et de log loss
     """
@@ -205,7 +208,7 @@ def gestion_erreur(res, test_y, labels, classifieur):
 
     return res
 
-def etablir_liste_chiens():
+def cnn_etablir_liste_chiens():
     """
     Création de la liste aléatoire des chiens pour les races selectionnés
     """
@@ -219,7 +222,7 @@ def etablir_liste_chiens():
     cpt_race = 0
 
     # Création de la liste aléatoire des races
-    liste_chiens = os.listdir(DOSSIER_SOURCE)
+    liste_chiens = os.listdir(IMG_DIR)
     #liste_chiens = [x for x in liste_chiens if "." not in x]
 
     for i in range(0, NB_RACES):
@@ -233,13 +236,13 @@ def etablir_liste_chiens():
         cpt_exemple = 0
         if cpt_race < NB_RACES+1:
             cpt_race = cpt_race+1
-            for filename in os.listdir(DOSSIER_SOURCE + dirs):
+            for filename in os.listdir(IMG_DIR + dirs):
                 # On ne garde que NB_EXEMPLES exemplaires de chaque race
                 if cpt_exemple < NB_EXEMPLES:
                     cpt_exemple = cpt_exemple+1
 
                     # Chemin complet de l'image
-                    #liste_images.append(IMG_DIR + dirs + '/' + filename)
+                    #liste_images.append(DOSSIER_SOURCE + dirs + '/' + filename)
                     liste_images.append(filename)
 
                     # Rajout du label
@@ -249,9 +252,9 @@ def etablir_liste_chiens():
     liste_images['labels'] = labels
     liste_images.to_csv('/home/toni/Bureau/liste.csv')
 
-def main():
+def cnn_recup_images():
     """
-    Fonction principale
+    Fonction de récupération des images
     """
 
     # Listes vides
@@ -260,7 +263,7 @@ def main():
     x_test = []
 
     # Etablissement du dataset de manière aléatoire
-    etablir_liste_chiens()
+    cnn_etablir_liste_chiens()
 
     #liste_images.to_csv('/home/toni/Bureau/liste.csv')
     liste_images = pd.read_csv('/home/toni/Bureau/liste.csv')
@@ -282,7 +285,7 @@ def main():
     i = 0
     for file, dump in tqdm(liste_train.values):
         # Lecture de l'image
-        img = cv2.imread(IMG_DIR + file)
+        img = cv2.imread(DOSSIER_SOURCE + file)
 
         # Rajout des données dans la liste
         x_train.append(cv2.resize(img, (T_IMG, T_IMG)))
@@ -294,10 +297,22 @@ def main():
     # Récupération des images de testing
     for file in tqdm(liste_test['liste'].values):
         # Lecture de l'image
-        img = cv2.imread(IMG_DIR + file)
+        img = cv2.imread(DOSSIER_SOURCE + file)
 
         # Rajout des données dans la liste
         x_test.append(cv2.resize(img, (T_IMG, T_IMG)))
+
+    return liste_train, liste_test, x_train, y_train, x_test
+
+def main():
+    """
+    Fonction principale
+    """
+
+    # Debut du decompte du temps
+    start_time = time.time()
+
+    liste_train, liste_test, x_train, y_train, x_test = cnn_recup_images()
 
     # Reformatage des listes pour le format de VGG19
     y_train_raw = np.array(y_train, np.uint8)
@@ -314,12 +329,12 @@ def main():
                                                           random_state=1)
 
     # Appel de la fonction de transfer learning
-    model = appel_vgg(x_train,
-                      y_train,
-                      x_valid,
-                      y_valid,
-                      liste_train,
-                      liste_test)
+    model = cnn_appel_vgg(x_train,
+                          y_train,
+                          x_valid,
+                          y_valid,
+                          liste_train,
+                          liste_test)
 
     # Calcul des résultats
     predictions = model.predict(x_valid)
@@ -334,10 +349,10 @@ def main():
     # Gestion d'une erreur
     if len(res.columns) != NB_RACES:
         res = gestion_erreur(res, predictions, liste_train['labels'], 'cnn')
-    calcul_resultats(res, np.asarray(predictions), 'cnn')
+    cnn_calcul_resultats(res, np.asarray(predictions), 'cnn')
 
     errors = np.where(predictions != truth)[0]
-    print("No of errors =", len(errors), "/", len(liste_test))
+    print("No of errors =", len(errors), "/", len(predictions))
 
 #    # Visualisation des images mal labelisées
 #    # Check Performance
@@ -360,6 +375,9 @@ def main():
 #            pred_label,
 #            prob[errors[i]][pred_class]))
 #
-#        original = load_img('{}/{}'.format(IMG_DIR, fnames[errors[i]]))
+#        original = load_img('{}/{}'.format(DOSSIER_SOURCE, fnames[errors[i]]))
 #        plt.imshow(original)
 #        plt.show()
+    # Affichage du temps d execution
+
+    print("Temps d execution : %s secondes ---" % (time.time() - start_time))
