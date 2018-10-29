d423900cda9c823b36e5a9b989d9abab8ce44a34
toni <p.froide@gmail.com>
2018-10-29 20:50:16 +0100

    Prévisions simple avec modèle pré entrainé. Nécessite deux fichiers supplémentaires et est limité aux 10 races pré-entrainées

--- Projet_7/p7_prevision_simple.py
+++ Projet_7/p7_prevision_simple.py
@@ -6,11 +6,22 @@ Created on Sun Oct 28 22:39:52 2018
 @author: toni
 """
 
+# Librairies
+import warnings
+import numpy as np
+import pandas as pd
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
 from keras.applications.vgg19 import preprocess_input
 from keras.applications.vgg19 import decode_predictions
 from keras.applications.vgg19 import VGG19
+from sklearn.externals import joblib
+
+# Pour ne pas avoir les warnings lors de la compilation
+warnings.filterwarnings("ignore")
+
+# Variables globales
+DOSSIER_SAVE = '/home/toni/python/python/Projet_7/images/'
 
 def main(fichier):
     """
@@ -18,12 +29,15 @@ def main(fichier):
     """
 
     # On crée un modèle déjà pré entrainé
-    model = VGG19(weights="imagenet", include_top=True)
+    model = joblib.load(DOSSIER_SAVE + 'savefit.pkl')
+    liste_noms = pd.read_csv(DOSSIER_SAVE + 'savefit.csv')
+    del liste_noms['Unnamed: 0']
+    liste_noms = liste_noms.columns
 
     # Visualisation de toutes les couches du modèle
     model.summary()
 
-    # On va chercher l'imagenversion d
+    # On va chercher l'image
     image = load_img(fichier, target_size=(224, 224))
 
     # Convertion de l'image
@@ -37,10 +51,8 @@ def main(fichier):
 
     # Prediction (des probabilités)
     proba = model.predict(image)
-    label = decode_predictions(proba)
-
-    # Label avec la plus grande probabilité
-    label = label[0][0]
+    proba = pd.DataFrame(proba, columns=liste_noms)
+    label = np.argmax(proba.loc[0], axis=1)
 
     # Affichage
-    print('Race :', label[1], 'avec une probabilité de', round((label[2]*100), 2), '%')
+    print('Race la plus probable :', label)
